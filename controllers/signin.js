a4c293d675237eaf480d1bef511a8337e6427ff6
mohit2308jain <mohitjain5000@gmail.com>
2020-07-14 02:41:23 +0530

    JWT

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,60 +1,74 @@
-const handleSignin = (req, res, db, bcrypt) => {
-    const { email, password } = req.body;
-    if(!email || !password){
-        return res.status(400).json('Incorrect form submission.')
-    }
+const auth = require("./auth");
 
-    db.select('email', 'hash').from('login')
-        .where('email', '=', email)
-        .then((data) => {
-            const isValid = bcrypt.compareSync(password, data[0].hash);
-            if(isValid){
-                return db.select('*').from('users')
-                    .where('email', '=', email)
-                    .then((user) => {
-                        res.json(user[0])
-                    })
-                    .catch((err) => res.status(400).json('Unable to get user'));
+const JWT_SECRET = "mohit";
+
+const handleSignin = (req, res, db, bcrypt, jwt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Incorrect form submission.");
+  }
+
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            const token = jwt.sign({ id: user[0].id }, JWT_SECRET, {
+              expiresIn: 3600,
+            });
+            if (!token) {
+              throw Error("Couldnt sign the token");
             }
-            else{
-                res.status(400).json('Wrong credentials');
-            }
-        })
-        .catch((err) => res.status(400).json('Wrong credentials'));
-}
+            console.log(token);
+            res.json({ token, user: user[0] });
+          })
+          .catch((err) => res.status(400).json("Unable to get user"));
+      } else {
+        res.status(400).json("Wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("Wrong credentials"));
+};
 
 const getBudget = (req, res, db) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    db.select('budget').from('users')
-        .where('id', '=', id)
-        .then((budget) => {
-            res.json(budget[0]);
-        })
-        .catch((err) => res.status(400).json('Not available'));
-}
+  db.select("budget")
+    .from("users")
+    .where("id", "=", id)
+    .then((budget) => {
+      res.json(budget[0]);
+    })
+    .catch((err) => res.status(400).json("Not available"));
+};
 
 const updateBudget = (req, res, db) => {
-    const { id, budget } = req.body;
-    db('users')
-        .where('id', '=', id)
-        .update({
-            budget: budget
-        })
-        .returning('budget')
-        .then((budget) => {
-            if(budget.length){
-                res.json(budget[0]);
-            }
-            else{
-                res.status(404).json('User Not Found');
-            }
-        })
-        .catch((err) => res.status(400).json('Unable to get entries'));
-}
+  const { id, budget } = req.body;
+  console.log(id, budget)
+  db("users")
+    .where("id", "=", id)
+    .update({
+      budget: budget,
+    })
+    .returning("budget")
+    .then((budget) => {
+      if (budget.length) {
+        res.json(budget[0]);
+      } else {
+        res.status(404).json("User Not Found");
+      }
+    })
+    .catch((err) => res.status(400).json("Unable to get entries"));
+};
 
 module.exports = {
-    handleSignin: handleSignin,
-    updateBudget: updateBudget,
-    getBudget: getBudget
-}
+  handleSignin: handleSignin,
+  updateBudget: updateBudget,
+  getBudget: getBudget,
+};
--- server.js
+++ server.js
@@ -1,85 +1,88 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
-const knex = require('knex');
+require("dotenv").config();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const knex = require("knex");
+const jwt = require("jsonwebtoken");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const expense = require('./controllers/expenses');
-const profile = require('./controllers/profile');
+const auth = require("./controllers/auth");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const expense = require("./controllers/expenses");
+const profile = require("./controllers/profile");
 
 /*
 const db = knex({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'mohit',
-        database: 'expenser'
-    }
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "mohit",
+    database: "expenser",
+  },
 });
 */
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Working');
-})
-
-app.post('/signin', (req, res) => {
-    signin.handleSignin(req, res, db, bcrypt);
-})
-
-app.post('/register', (req, res) => {
-    register.handleRegister(req, res, db, bcrypt);
+app.get("/", (req, res) => {
+  res.send("Working");
 });
 
-app.get('/budget/:id', (req, res) => {
-    signin.getBudget(req, res, db);
-})
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt, jwt);
+});
 
-app.put('/budget', (req, res) => {
-    signin.updateBudget(req, res, db);
-})
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
 
-app.get('/expense/:id', (req, res) => {
-    expense.getExpense(req, res, db);
-})
+app.get("/budget/:id", (req, res) => {
+  signin.getBudget(req, res, db);
+});
 
-app.put('/expense/:id', (req, res) => {
-    expense.updateExpense(req, res, db);
-})
+app.put("/budget", (req, res) => {
+  signin.updateBudget(req, res, db);
+});
 
-app.delete('/expense/:id', (req, res) => {
-    expense.deleteExpense(req, res, db);
-})
+app.get("/expense/:id", (req, res) => {
+  expense.getExpense(req, res, db);
+});
 
-app.post('/expense', (req, res) => {
-    expense.addExpense(req, res, db);
-})
+app.put("/expense/:id", (req, res) => {
+  expense.updateExpense(req, res, db);
+});
 
-app.get('/expenses/:id', (req,res) => {
-    expense.getExpenses(req, res, db);
-})
+app.delete("/expense/:id", (req, res) => {
+  expense.deleteExpense(req, res, db);
+});
 
-app.get('/profile/:id', (req, res) => {
-    profile.getProfile(req, res, db);
-})
+app.post("/expense", (req, res) => {
+  expense.addExpense(req, res, db);
+});
 
+app.get("/expenses/:id", (req, res) => {
+  expense.getExpenses(req, res, db);
+});
+
+app.get("/profile/:id", auth.auth, (req, res) => {
+  profile.getProfile(req, res, db);
+});
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`App running on port ${process.env.PORT}`);
-})
+  console.log(`App running on port ${process.env.PORT}`);
+});
